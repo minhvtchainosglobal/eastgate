9b000beae9275eb93ac4e94fd5d9c49a70bbcf61
minhvt <minhvu1661@gmail.com>
2025-03-24 22:16:00 +0700

    1st commit

---
+++ src/main/java/com/eastgate/user_key_management/UserKeyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.eastgate.user_key_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserKeyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserKeyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eastgate/user_key_management/UserKeyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastgate.user_key_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserKeyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
