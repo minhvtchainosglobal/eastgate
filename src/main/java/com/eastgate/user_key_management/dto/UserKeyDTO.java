7acf933f3977b99636876bafa8a9790317c9e38a
minhvt <minhvu1661@gmail.com>
2025-03-25 00:02:16 +0700

    update Entity, DTO and pom.xml

--- src/main/java/com/eastgate/user_key_management/dto/UserKeyDTO.java
+++ src/main/java/com/eastgate/user_key_management/dto/UserKeyDTO.java
@@ -1,7 +1,16 @@
 package com.eastgate.user_key_management.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserKeyDTO {
     private UUID id;
     private String username;
